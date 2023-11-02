98d29e65e4ad8e828fdaf3404e8e9c7fc134c38a
HerrSchltn <luisscholten123@gmail.com>
2023-11-02 22:27:36 +0100

    added reference component

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,7 +1,5 @@
 /** @type {import("prettier").Config} */
 module.exports = {
-  // i am just using the standard config, change if you need something else
-
   pluginSearchDirs: [__dirname],
   plugins: [require.resolve("prettier-plugin-astro")],
   overrides: [
